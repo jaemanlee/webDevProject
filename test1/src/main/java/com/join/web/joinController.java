9bb52908b0b13998c5771a8cc1157839b759d966
lde1245 <lde1245 lde1245@naver.com>
2023-01-30 22:17:33 +0900

    joinController

--- test1/src/main/java/com/join/web/joinController.java
+++ test1/src/main/java/com/join/web/joinController.java
@@ -5,18 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/join")
+public class joinController {
+	private final Logger logger = LoggerFactory.getLogger(joinController.class.getName());
 
-
-	@Controller
-	@RequestMapping("/join")
-	public class joinController{
-		private final Logger logger = LoggerFactory.getLogger(joinController.class.getName()); 
-
-		@RequestMapping(value = "/comewithus")
-		public String comJoin() {
-			return "/main/";
-		}
-		
-	
+	@RequestMapping(value = "/comewithus.do")
+	public String comJoin() {
+		return "/join/join";
+	}
 
 }
